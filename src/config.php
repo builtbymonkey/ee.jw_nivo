26908e9bae85c63687d88b4ddf832aa4cf351ce5
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-10-16 15:13:36 +1100

    Initial commit

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('JW_NIVO_VERSION')) {
+    define('JW_NIVO_NAME', 'JW Nivo');
+    define('JW_NIVO_VERSION', '0.1.0');
+}
+
+$config['name'] = JW_NIVO_NAME;
+$config['version'] = JW_NIVO_VERSION;
+$config['nsm_addon_updater']['versions_xml'] = 'http://complexcompulsions.com/add-ons/feed/jw-nivo/';
---
+++ src/ft.jw_nivo.php
@@ -0,0 +1,106 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+require PATH_THIRD.'jw_nivo/config.php';
+
+/**
+ * JW Nivo
+ *
+ * @package    jw_nivo
+ * @author     Jeremy Worboys <jw@jeremyworboys.com>
+ * @link       
+ * @copyright  Copyright (c) 2012 Jeremy Worboys
+ */
+class Jw_nivo_ft extends EE_Fieldtype {
+
+    /**
+     * Fieldtype Info
+     *
+     * @var array
+     */
+    public $info = array(
+        'name'    => JW_NIVO_NAME,
+        'version' => JW_NIVO_VERSION
+    );
+
+
+    /**
+     * Replace Tag
+     *
+     * This method replaces the field tag on the front-end.
+     *
+     * @param  array  The field data (or prepped data, if using pre_process)
+     * @param  array  The field parameters (if any)
+     * @param  string The data between tag (for tag pairs)
+     * @return string The text/HTML to replace the tag
+     */
+    public function replace_tag($data, $params=array(), $tagdata=FALSE)
+    {
+        // code...
+    }
+
+
+    /**
+     * Display Field
+     *
+     * This method runs when displaying the field on the publish page in the CP
+     *
+     * @param  array  The data previously entered into this field
+     * @return string The HTML output to be displayed for this field
+     */
+    public function display_field($field_data)
+    {
+        // code...
+    }
+
+
+    /**
+     * Prepare for Saving the Field
+     *
+     * This method prepares the data to be saved to the entries table in the
+     * database
+     *
+     * @param  array  The data entered into this field
+     * @return string The data to be stored in the database
+     */
+    public function save($data)
+    {
+        // code...
+    }
+
+
+    // -------------------------------------------------------------------------
+
+
+    /**
+     * Install
+     *
+     * Create any db tables
+     */
+    public function install()
+    {
+        // code...
+    }
+
+
+    /**
+     * Uninstall
+     *
+     * Remove any db tables
+     */
+    public function uninstall()
+    {
+        // code...
+    }
+
+
+    /**
+     * Update
+     *
+     * Update any db tables
+     */
+    public function update()
+    {
+        // code...
+    }
+}
+// END CLASS
