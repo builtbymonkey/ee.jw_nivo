28b626792aad86ff85c15a23202eb0362cf93caa
Jeremy Worboys <jw@jeremyworboys.com>
2013-05-09 08:01:05 +1000

    Increment version

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 
 if (!defined('JW_NIVO_VERSION')) {
     define('JW_NIVO_NAME', 'JW Nivo');
-    define('JW_NIVO_VERSION', '1.3.0');
+    define('JW_NIVO_VERSION', '1.3.1');
 }
 
 $config['name'] = JW_NIVO_NAME;
