ed42dbe9d33b6a06f0ca06ab35c07cf4b5ad6b36
Jeremy Worboys <jw@jeremyworboys.com>
2013-10-11 10:29:09 +1100

    Patch < EE 2.6

--- src/config.php
+++ src/config.php
@@ -8,3 +8,17 @@ if (!defined('JW_NIVO_VERSION')) {
 $config['name'] = JW_NIVO_NAME;
 $config['version'] = JW_NIVO_VERSION;
 $config['nsm_addon_updater']['versions_xml'] = 'http://jeremyworboys.com/add-ons/releases/jw-nivo/';
+
+
+/**
+ * < EE 2.6.0 backward compat
+ */
+if ( ! function_exists('ee'))
+{
+    function ee()
+    {
+        static $EE;
+        if ( ! $EE) $EE = get_instance();
+        return $EE;
+    }
+}
