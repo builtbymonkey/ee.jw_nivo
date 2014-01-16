45ddbc7e073a0bb96ec83469dc29860d7b1cdd2a
Jeremy Worboys <jw@jeremyworboys.com>
2014-01-16 22:05:59 +1100

    Bump version

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 
 if (!defined('JW_NIVO_VERSION')) {
     define('JW_NIVO_NAME', 'JW Nivo');
-    define('JW_NIVO_VERSION', '1.3.3');
+    define('JW_NIVO_VERSION', '1.3.4');
 }
 
 $config['name'] = JW_NIVO_NAME;
